b66c1d17b28c1af2e62a2fd722966928fa20486f
Michael-blaze <slashugaming@gmail.com>
2022-01-28 17:36:25 +0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+
+
+function main()
+{
+	let buttons = document.getElementsByClassName('button');
+	buttons = [...buttons];
+
+	buttons.forEach(function(button) {
+		console.log(button);
+	});
+}
+
+
+main();
