39276641b6aa330cea3956e6121c0f71489272c9
Michael-blaze <slashugaming@gmail.com>
2022-01-29 21:28:59 +0200

    Added event listeners for each buttons

--- js/script.js
+++ js/script.js
@@ -1,15 +1,72 @@
-let operand1 = "";
-let operand2 = "";
-let operator = "";
 
+
+function computeOperation(operand1, operand2, operator)
+{
+	switch(operator) 
+	{
+  		case "+":
+    		return Number(operand1) + Number(operand2);
+		case "-":
+		    return Number(operand1) - Number(operand2); 
+		case "*":
+			return Number(operand1) * Number(operand2);
+		case "/":
+			return Number(operand1) / Number(operand2);
+		case "%":
+			return Number(operand1) / 100 * Number(operand2);
+		default:
+		    return "Error";
+	}
+}
+
+
+function isOperator(operator)
+{
+	return operator == "+" || operator == "-" || operator == "*" || operator == "/" || operator == "%";
+}
 
 function main()
 {
-	let buttons = document.getElementsByClassName('button');
+	let operand1 = "";
+	let sign1    = "";
+	let operand2 = "";
+	let sign2    = "";
+	let operator = "";
+	let error    = "";
+	let buttons  = document.getElementsByClassName('button');
+	
+	const result_section = document.getElementById('result');
+
 	buttons = [...buttons];
 
 	buttons.forEach(function(button) {
-		console.log(button);
+		button.addEventListener('click', function() {
+			button_pressed = button.innerHTML;
+
+			if (button_pressed >= '0' && button_pressed <= '9' || button_pressed == ".")
+			{
+
+			}
+			else if (button_pressed == "AC")
+			{
+				
+			}
+			else if (button_pressed == "+/-")
+			{
+
+			}
+			else if (isOperator(button_pressed))
+			{
+				
+			}
+			else if (button_pressed == "=")
+			{
+				
+				
+
+			}
+
+		});
 	});
 }
 
